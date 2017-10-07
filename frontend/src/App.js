3b5e267ae19a001405d4ac25369e6c4295e0dbad
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2017-10-08 00:41:18 +0200

    Basic elements for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,76 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: null,
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <li>{this.props.firstname}</li>
+    );
+  }
+}
+
+class ContactList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contact: Array(9).fill(null),
+    };
+  }
+
+  renderContact(i) {
+    return (
+      <Contact firstname={i}
+
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="contact-row">
+          {this.renderContact("Ignacio")}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class ContactsApp extends React.Component {
+  render() {
+    return (
+      <div className="contactsapp">
+        <div className="contacslist">
+          <ContactList />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContactsApp;
